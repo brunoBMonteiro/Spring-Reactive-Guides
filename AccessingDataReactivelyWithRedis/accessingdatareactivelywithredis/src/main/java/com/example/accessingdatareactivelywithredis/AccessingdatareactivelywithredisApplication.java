9f3b4df61c87a15a00d4c339b58d2da5275ac694
brunoBMonteiro <brunobilheri@hotmail.com>
2022-06-29 13:35:26 -0300

    Accessing Data Reactively with Redis

---
+++ AccessingDataReactivelyWithRedis/accessingdatareactivelywithredis/src/main/java/com/example/accessingdatareactivelywithredis/AccessingdatareactivelywithredisApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatareactivelywithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatareactivelywithredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatareactivelywithredisApplication.class, args);
+	}
+
+}
---
+++ AccessingDataReactivelyWithRedis/accessingdatareactivelywithredis/src/test/java/com/example/accessingdatareactivelywithredis/AccessingdatareactivelywithredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatareactivelywithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdatareactivelywithredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
